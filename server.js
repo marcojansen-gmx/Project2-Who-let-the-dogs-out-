dcd9d98911fe74d7a6f83a74673dd92be2770ae2
Marco <Marco.jansen@gmx.us>
2021-03-04 20:46:06 +0800

    deleted index.html

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ app.set('view engine', 'handlebars');
 htmlRouter(app);
 apiRouter(app);
 
-// Syncing our sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+  });
 });
