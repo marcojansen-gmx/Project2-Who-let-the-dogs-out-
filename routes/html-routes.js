f151a1897c8bf918d2df1ece355ffb1a226bde31
n-flam <nadja.flam@gmx.net>
2021-03-08 18:44:03 +0800

    handlebars for signup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,12 +13,12 @@ module.exports = function (app) {
     });
   });
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "public", "index.html"));
-  });
+  
 
   app.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, "../", "public", "html", "signup.html"));
+    res.render("signup", {
+      showNav: false
+    });
   });
 
   app.get("/login", (req, res) => {
