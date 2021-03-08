f6388c9a760fe5def97d9a94d43ffc0ed9fd6907
Marco <Marco.jansen@gmx.us>
2021-03-08 20:18:08 +0800

    image into db via signup, redirect to login, changed to longblob in modules and changed sex in modules to boolean

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,31 @@
 
 $(document).ready(function () {
   // Getting references to our form and input
-  const signUpForm = $("form.signup");
-  const firstNameInput = $("input#firstNameInput");
-  const lastNameInput = $("input#lastNameInput");
-  const postCodeInput = $("input#postCode");
-  const emailInput = $("input#exampleInputEmail1");
-  const passwordInput = $("input#exampleInputPassword1");
-  const dogNameInput = $("input#dogNameInput");
-  const dogAgeInput = $("input#dogAgeInput");
-  const dogBreedInput = $("#dogBreedInput");
-  // const dogSexInput = $("input#dogSexInput");
-  // const dogDesexedInput = $("input#dogDesexedInput");
-  const dogAllergiesInput = $("input#dogAllergiesInput");
-  // const dogChildFriendlyInput = $("input#childFriendlyInput");
-  // const dogPhotoInput = $("input#dogPhotoInput");
-  const dogDescriptionInput = $("#dogDescriptionInput")
+  const signUpForm = $('form.signup');
+  const firstNameInput = $('input#firstNameInput');
+  const lastNameInput = $('input#lastNameInput');
+  const postCodeInput = $('input#postCode');
+  const emailInput = $('input#exampleInputEmail1');
+  const passwordInput = $('input#exampleInputPassword1');
+  const dogNameInput = $('input#dogNameInput');
+  const dogAgeInput = $('input#dogAgeInput');
+  const dogBreedInput = $('#dogBreedInput');
+  // const dogSexInput = $('input#dogSexInput');
+  // const dogDesexedInput = $('input#dogDesexedInput');
+  const dogAllergiesInput = $('input#dogAllergiesInput');
+  // const dogChildFriendlyInput = $('input#childFriendlyInput');
+  // const dogPhotoInput = $('input#dogPhotoInput');
+  const dogDescriptionInput = $('#dogDescriptionInput')
 
   let file;
 
   // When the signup button is clicked, we validate the email and password are not blank
 
-  $('input[type="file"]').change(function (e) {
+  $('input[type='file']').change(function (e) {
     file = e.target.files[0];
   });
 
-  signUpForm.on("submit", function (event) {
+  signUpForm.on('submit', function (event) {
     event.preventDefault();
     const userData = {
       firstName: firstNameInput.val().trim(),
@@ -36,38 +36,41 @@ $(document).ready(function () {
       dogName: dogNameInput.val().trim(),
       age: dogAgeInput.val().trim(),
       breed: dogBreedInput.val().trim(),
-      sex: $("input[name='sex']:checked").val(),
-      desexed: $("input[name='desexed']:checked").val(),
+      sex: $('input[name='sex']:checked').val(),
+      desexed: $('input[name='desexed']:checked').val(),
       allergies: dogAllergiesInput.val().trim(),
-      childFriendly: $("input[name='childFriendly']:checked").val(),
+      // childFriendly: $('input[name='childFriendly']:checked').val(),
       // dogImage: dogPhotoInput.val().trim(),
       userText: dogDescriptionInput.val().trim(),
       // image: file
     };
-    // debugger;
-    // if (!userData.firstName || !userData.lastName || userData.postCode || !userData.email || !userData.password || !userData.dogName || !userData.age || !userData.breed || !userData.sex || !userData.desexed || !userData.allergies || !userData.childFriendly || !userData.dogImage || !userData.userText) {
 
-    //   alert("All Fields Must be Entered to Sign Up!!!")
+    const formData = new FormData();
+    Object.keys(userData).map((key) => {
+      formData.append(key, userData[key]);
+    });
 
-    //   return;
-    // }
+    formData.append('dogImage', file);
 
-    $.post("/api/signup", userData).then(function (data) {
-      window.location.replace("/html/login.html");
-      // If there's an error, handle it by throwing up a bootstrap alert
-    })
-      .catch(handleLoginErr);
-    // If we have an email and password, run the signUpUser function
-    // signUpUser(userData.email, userData.password);
-    // emailInput.val("");
-    // passwordInput.val("");
+    $.ajax({
+      url: '/api/signup',
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+      method: 'POST',
+      type: 'POST', // For jQuery < 1.9
+      success: function () {
+        window.location.replace('/html/login.html');
+      }
+    });
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
 
 
   function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
+    $('#alert .msg').text(err.responseJSON);
+    $('#alert').fadeIn(500);
   }
 });
